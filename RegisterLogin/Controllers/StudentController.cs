4adb740f3fea86f82e8f02ffc55e88f4875d099b
arman <armanansari@gmail.com>
2025-06-23 13:28:20 +0530

    add new create

--- RegisterLogin/Controllers/StudentController.cs
+++ RegisterLogin/Controllers/StudentController.cs
@@ -8,5 +8,9 @@ namespace RegisterLogin.Controllers
         {
             return View();
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
